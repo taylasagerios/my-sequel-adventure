fdd46c7dd7042966979eae4ed256dfedcc31e147
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-11-08 13:22:59 -0800

    fixed the code for controller api index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,43 +1,12 @@
+const router = require("express").Router();
 const User = require("./userRoutes");
 const Character = require("./characterRoutes");
 const CharacterPotion = require("./characterPotionRoutes");
 const Potion = require("./potionRoutes");
 
-User.hasMany(Character, {
-    foreignKey: "user_id",
-    onDelete: "CASCADE",
-});
+router.use("/users", userRoutes);
+router.use("/character", characterRoutes);
+router.use("/characterPotion", characterPotionRoutes);
+router.use("/potion", potionRoutes);
 
-Character.belongsTo(User, {
-    foreignKey: "user_id",
-});
-
-User.hasMany(CharacterPotion , {
-    foreignKey: "user_id",
-    onDelete: "CASCADE",
-});
-
-CharacterPotion .belongsTo(User, {
-    foreignKey: "user_id",
-});
-
-CharacterPotion .belongsTo(Character, {
-    foreignKey: "blogPost_id",
-    onDelete: "CASCADE",
-});
-
-Potion.belongsTo(User, {
-    foreignKey: "user_id",
-});
-
-Potion.belongsTo(Character, {
-    foreignKey: "blogPost_id",
-    onDelete: "CASCADE",
-});
-
-Character.hasMany(CharacterPotion , {
-    foreignKey: "blogPost_id",
-    onDelete: "CASCADE",
-});
-
-module.exports = { User, Character, CharacterPotion,Potion };
+module.exports = router;
